5330b8fd7e6758448e2cdf6db844381f1de43e1d
a5o <alberto.ferrarini@gmail.com>
2021-02-22 08:15:31 +0100

    removed capture keycode and added hyper in place of left control

--- keyboards/atreus62/keymaps/a5o/keymap.c
+++ keyboards/atreus62/keymaps/a5o/keymap.c
@@ -15,8 +15,7 @@ enum custom_keycodes {
 	TMUXL,
 	TMUXK,
 	TMUXJ,
-	TMUXH,
-	CAPTURE
+	TMUXH
 };
 
 enum unicode_names {
@@ -72,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, 
 	LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 	OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), 
-	KC_LCTL, MO(_FUN), KC_LGUI, KC_LALT, RAISE, LT(_DIR,KC_SPC), KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+	KC_HYPR, MO(_FUN), KC_LGUI, KC_LALT, RAISE, LT(_DIR,KC_SPC), KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
 	),
 
 	[_LOWER] = LAYOUT(
@@ -102,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FUN] = LAYOUT(
 	KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, 
 	KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-	KC_TRNS, KC_NO, CAPTURE, KC_NO, KC_NO, KC_NO, TMUXH, TMUXJ, TMUXK, TMUXL, KC_NO, KC_NO, 
+	KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TMUXH, TMUXJ, TMUXK, TMUXL, KC_NO, KC_NO, 
 	KC_TRNS, KC_BRID, KC_BRIU, KC_NO, KC_NO, KC_NO, NUMPAD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
 	),
@@ -177,11 +176,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			if (record->event.pressed) {
 			SEND_STRING(SS_LCTL("a"));
 			SEND_STRING("l");
-			}
-			break; 
-		case CAPTURE:
-			if (record->event.pressed) {
-			SEND_STRING(SS_LSFT(SS_LGUI("5")));
 			}
 			break; 
 	}
