d21bce8d3cc54f33b835ce208b2294406b6922c6
Alberto <alberto.ferrarini@gmail.com>
2024-10-27 07:47:41 +0100

    added adjust layer and macros for accented characters

--- keyboards/atreus62/keymaps/a5o/keymap.c
+++ keyboards/atreus62/keymaps/a5o/keymap.c
@@ -11,6 +11,22 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _FUN 3
+#define _ADJUST 4
+
+enum custom_keycodes {
+  LOWER=SAFE_RANGE,
+  RAISE,
+	TMUXL,
+	TMUXK,
+	TMUXJ,
+	TMUXH,
+	EGRAVE,
+	EACU,
+	AGRAVE,
+	UGRAVE,
+	IGRAVE,
+	OGRAVE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
@@ -18,20 +34,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, 
 	LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 	OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), 
-	KC_LCTL, MO(_FUN), KC_LGUI, KC_LALT, MO(_RAISE), KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, MO(_LOWER), KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+	KC_LCTL, MO(_FUN), KC_LGUI, KC_LALT, RAISE, KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
 	[_LOWER] = LAYOUT(
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_INS, KC_END, KC_PGUP, KC_TRNS,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_DOWN, KC_TRNS,
+	KC_TRNS, KC_TRNS, KC_TRNS, EGRAVE, KC_TRNS, KC_TRNS, KC_TRNS, UGRAVE, IGRAVE, OGRAVE, KC_TRNS, KC_TRNS,
+	KC_TRNS, AGRAVE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TMUXH, TMUXJ, TMUXK, TMUXL, KC_TRNS, KC_TRNS,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 	),
 
 	[_RAISE] = LAYOUT(
 	KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS, 
-	KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_UNDS, 
+	KC_TRNS, KC_NO, KC_NO, EACU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_UNDS, 
 	KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_DQUO, 
 	KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_QUES, KC_PIPE, 
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
@@ -45,6 +61,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
 	),
 
+	[_ADJUST] = LAYOUT(
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, QK_BOOT, EE_CLR, DB_TOGG, KC_NO, KC_NO, AG_SWAP, AG_NORM, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+	)
   /*
   [_NAV] = LAYOUT(
     TO(_DEFAULT),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
@@ -71,3 +94,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   */
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+		case LOWER:
+			if (record->event.pressed) {
+				layer_on(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}else{
+				layer_off(_LOWER);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+		case RAISE:
+			if (record->event.pressed) {
+				layer_on(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}else{
+				layer_off(_RAISE);
+				update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break; 
+		case TMUXH:
+			if (record->event.pressed) {
+      SEND_STRING(SS_LCTL("b"));
+			SEND_STRING("h");
+			}
+			break; 
+		case TMUXJ:
+			if (record->event.pressed) {
+      SEND_STRING(SS_LCTL("b"));
+			SEND_STRING("j");
+			}
+			break; 
+		case TMUXK:
+			if (record->event.pressed) {
+      SEND_STRING(SS_LCTL("b"));
+			SEND_STRING("k");
+			}
+			break; 
+		case TMUXL:
+			if (record->event.pressed) {
+      SEND_STRING(SS_LCTL("b"));
+			SEND_STRING("l");
+			}
+			break; 
+		case EGRAVE:
+			if (record->event.pressed) {
+			SEND_STRING("`e");
+			}
+			break; 
+		case EACU:
+			if (record->event.pressed) {
+			SEND_STRING("'e");
+			}
+			break; 
+		case AGRAVE:
+			if (record->event.pressed) {
+			SEND_STRING("`a");
+			}
+			break; 
+		case UGRAVE:
+			if (record->event.pressed) {
+			SEND_STRING("`u");
+			}
+			break; 
+		case IGRAVE:
+			if (record->event.pressed) {
+			SEND_STRING("`i");
+			}
+			break; 
+		case OGRAVE:
+			if (record->event.pressed) {
+			SEND_STRING("`o");
+			}
+			break; 
+	}
+	return true;
+}
