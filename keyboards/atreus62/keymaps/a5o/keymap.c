8e6cceb7712ca5dbfce981970a725fb5f20ccf0c
Alberto <alberto.ferrarini@gmail.com>
2024-11-04 20:01:05 +0100

    added dynamic macros

--- keyboards/atreus62/keymaps/a5o/keymap.c
+++ keyboards/atreus62/keymaps/a5o/keymap.c
@@ -11,7 +11,8 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _FUN 3
-#define _ADJUST 4
+#define _MACRO 4
+#define _ADJUST 5
 
 enum custom_keycodes {
   LOWER=SAFE_RANGE,
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, 
 	LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 	OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), 
-	KC_LCTL, MO(_FUN), KC_LGUI, KC_LALT, RAISE, KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+	MO(_MACRO), MO(_FUN), KC_LGUI, KC_LALT, RAISE, KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
 	[_LOWER] = LAYOUT(
@@ -65,6 +66,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRNS, KC_BRID, KC_BRIU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
 	),
+
+  [_MACRO] = LAYOUT(
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , DM_REC1  , DM_REC2  , DM_RSTP  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , DM_PLY1  , DM_PLY2  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
+  ),
 
 	[_ADJUST] = LAYOUT(
 	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
