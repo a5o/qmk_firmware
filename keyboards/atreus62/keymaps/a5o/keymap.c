790e9af600a717d29b6df9dc9e29589f373e801f
Alberto <alberto.ferrarini@gmail.com>
2025-08-08 20:33:21 +0200

    disabled dynamic macro feature; added compose key

--- keyboards/atreus62/keymaps/a5o/keymap.c
+++ keyboards/atreus62/keymaps/a5o/keymap.c
@@ -11,8 +11,7 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _FUN 3
-#define _MACRO 4
-#define _ADJUST 5
+#define _ADJUST 4
 
 enum unicode_names {
 		EACU,
@@ -93,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, 
 	LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 	KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-	MO(_MACRO), MO(_FUN), KC_LGUI, KC_LALT, RAISE, KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+	KC_RALT, MO(_FUN), KC_LGUI, KC_LALT, RAISE, KC_SPC, KC_BSPC, RCTL_T(KC_ENT), KC_SPC, LOWER, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
   ),
 
 	[_LOWER] = LAYOUT(
@@ -119,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TRNS, KC_BRID, KC_BRIU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_MNXT
 	),
-
+/*
   [_MACRO] = LAYOUT(
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , DM_REC1  , DM_REC2  , DM_RSTP  , KC_NO  , KC_NO   ,
@@ -127,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
   ),
-
+*/
 	[_ADJUST] = LAYOUT(
 	KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
 	KC_NO, QK_BOOT, EE_CLR, DB_TOGG, KC_NO, DT_PRNT, AG_SWAP, AG_NORM, KC_NO, KC_NO, KC_NO, KC_NO, 
